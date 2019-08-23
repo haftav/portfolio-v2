56f870a616c8f98ff089be24f351bcc7d2b8a0f7
Tav Hafner <23143092+haftav@users.noreply.github.com>
2019-08-23 13:36:12 -0600

    working on fluid titles

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
+import SplashDiagonal from './Splash/SplashDiagonal';
+
 export default () => {
   return (
     <div className="splash">
       <div className="splash__headerContainer">
         <h1 className="splash__header">Hi, I'm Tav.</h1>
       </div>
-      <div className="splash__subheaderContainer">
+      {/* <div className="splash__subheaderContainer">
         <h3 className="splash__subheader">
           I specialize in creating optimized and elegant web experiences.
         </h3>
       </div>
+      <div className='splash__diagonalContainer'>
+        <SplashDiagonal />
+      </div> */}
     </div>
   )
 }
---
+++ src/components/Splash/SplashDiagonal.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1920"
+    height="782.752"
+    viewBox="0 0 1920 782.752"
+  >
+    <path
+      d="M4510.324-925.616v386.379h1920v-782.752Z"
+      transform="translate(-4510.324 1321.989)"
+      fill="#fff"
+    />
+  </svg>
+)
