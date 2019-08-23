7b303f6104a30688699d03aa5739249ec6e4ff09
Tav Hafner <23143092+haftav@users.noreply.github.com>
2019-08-23 13:53:41 -0600

    progress on splash titles

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
-import SplashDiagonal from './Splash/SplashDiagonal';
+import SplashDiagonal from './Splash/SplashDiagonal'
 
 export default () => {
   return (
     <div className="splash">
       <div className="splash__headerContainer">
-        <h1 className="splash__header">Hi, I'm Tav.</h1>
+        <h1 className="splash__header">Hi,<br /> I'm Tav.</h1>
       </div>
-      {/* <div className="splash__subheaderContainer">
-        <h3 className="splash__subheader">
-          I specialize in creating optimized and elegant web experiences.
-        </h3>
+      <div className="splash__subheaderContainer">
+        <div className="splash__subheaderWrapper">
+          <h3 className="splash__subheader">
+            I specialize in creating optimized and elegant web experiences.
+          </h3>
+        </div>
       </div>
-      <div className='splash__diagonalContainer'>
+      {/* <div className='splash__diagonalContainer'>
         <SplashDiagonal />
       </div> */}
     </div>
