08b74ac2a759ec53df74a81e219c52352f9e948b
Tav Hafner <23143092+haftav@users.noreply.github.com>
2019-08-24 11:25:12 -0600

    starting on tablet

--- src/components/Splash/SplashDiagonal.jsx
+++ src/components/Splash/SplashDiagonal.jsx
@@ -4,7 +4,7 @@ export default () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 1920 782.752"
-    className="testy-test"
+    className="splashDiagonal"
   >
     <path
       d="M4510.324-925.616v386.379h1920v-782.752Z"
