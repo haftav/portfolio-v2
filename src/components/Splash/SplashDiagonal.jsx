9460c16f7c1a06b20094a4bf2e4e7713e748e27c
Tav Hafner <23143092+haftav@users.noreply.github.com>
2019-08-25 11:30:00 -0600

    title responsiveness for splash is coming together

--- src/components/Splash/SplashDiagonal.jsx
+++ src/components/Splash/SplashDiagonal.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
+import Diagonal from './Diagonal.svg'
 
-export default () => (
+const SmallDiagonal = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 1920 782.752"
-    className="splashDiagonal"
+    className="splashDiagonal splashDiagonal--small"
   >
     <path
       d="M4510.324-925.616v386.379h1920v-782.752Z"
@@ -12,4 +13,27 @@ export default () => (
       fill="#fff"
     />
   </svg>
+)
+
+const LargeDiagonal = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 3840 1565.504"
+    className="splashDiagonal splashDiagonal--large"
+    preserveAspectRatio="none"
+  >
+    <path
+      d="M4510.325-529.243V243.515h3840v-1565.5Z"
+      transform="translate(-4510.325 1321.989)"
+      fill="#fff"
+    />
+  </svg>
+)
+
+export default () => (
+  <>
+    <SmallDiagonal />
+    <LargeDiagonal />
+  </>
+  // <img src={SmallDiagonal} alt="diagonal" className="diagonal"/>
 )
