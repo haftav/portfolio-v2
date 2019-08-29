f4f6e818e8450e2b310cf1f18036f2cb20193bb8
Tav Hafner <23143092+haftav@users.noreply.github.com>
2019-08-28 19:48:32 -0600

    working on mobile about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from 'react'
 
 export default () => {
-    return (
-        <div className='about'>
-            about
-        </div>
-    )
+  return (
+    <div className="about">
+      <h2 className="about__header">About Me</h2>
+      <div className="about__divider"/>
+      <div className="about__textContainer">
+        <p className="about__paragraph">
+          I’m a professional web developer with experience building and
+          maintaining enterprise-level applications. My range of knowledge lies
+          across the full stack of development, from creating rich and robust
+          user interfaces to building and testing APIs. I currently work on the
+          front end team at Overstock.com, where my contributions to projects
+          have been estimated to save 600+ hours of work per year.{' '}
+        </p>
+        <p className="about__paragraph">
+          When I’m not coding, I enjoy playing guitar, watching movies, hiking,
+          biking and skiing in the incredible state of Utah.
+        </p>
+      </div>
+    </div>
+  )
 }
