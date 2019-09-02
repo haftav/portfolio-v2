33e2fae83b32d2a673795782d7fab4be3597b401
Tav Hafner <23143092+haftav@users.noreply.github.com>
2019-09-02 16:54:36 -0600

    skills grid seems to be working for mobile

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,6 +10,32 @@ export default () => {
       <div className="skills__colorOverlay" />
       <div className="skills__content">
         <h2 className="skills__header">Skills</h2>
+        <div className="skills__list">
+          <div className="skills__listItem">
+            <span className="skills__listSpan">JAVASCRIPT</span>
+          </div>
+          <div className="skills__listItem">
+            <span className="skills__listSpan">REACT</span>
+          </div>
+          <div className="skills__listItem">
+            <span className="skills__listSpan">HTML</span>
+          </div>
+          <div className="skills__listItem">
+            <span className="skills__listSpan">CSS/SASS/LESS</span>
+          </div>
+          <div className="skills__listItem">
+            <span className="skills__listSpan">NODE</span>
+          </div>
+          <div className="skills__listItem">
+            <span className="skills__listSpan">TESTING</span>
+          </div>
+          <div className="skills__listItem">
+            <span className="skills__listSpan">BUILDING</span>
+          </div>
+          <div className="skills__listItem">
+            <span className="skills__listSpan">SQL</span>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/Skills/SkillsDivider.jsx
+++ src/components/Skills/SkillsDivider.jsx
@@ -3,23 +3,29 @@ import React from 'react'
 const SkillsDivider = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 1919.842 200.965"
+    viewBox="0 0 1919.207 213"
     preserveAspectRatio="none"
     className="skills__divider"
   >
-    <g transform="translate(222.784 -3329.035)">
+    <g transform="translate(223 -3317)">
+      <rect
+        width="1919"
+        height="14"
+        transform="translate(-223 3317)"
+        fill="#fff"
+      />
       <path
         d="M809.051,2602.014l753.028-122.905V2679Z"
         transform="translate(134 851)"
         fill="rgba(0,188,131,0.44)"
       />
       <path
-        d="M-356.784,2679V2478.035l611.56,65.875Z"
+        d="M-356.784,2679l-.033-200,611.593,64.911Z"
         transform="translate(134 851)"
         fill="rgba(37,90,74,0.45)"
       />
       <path
-        d="M-356.784,2479,809.524,2602.035l753.535-122.821Z"
+        d="M-356.784,2479,809.524,2602.035l752.684-122.983Z"
         transform="translate(134 851)"
         fill="#fff"
       />
