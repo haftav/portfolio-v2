1e1e1220ff865fc787470d65efdafe16952f2303
Tav Hafner <23143092+haftav@users.noreply.github.com>
2019-09-02 15:05:49 -0600

    found a fix for background image overlay

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 export default () => {
-    return (
-        <div className='skills'>
-            skills
-        </div>
-    )
+  return (
+    <div className="skills">
+      <div className="skills__colorOverlay" />
+      <div className="skills__content">
+        <h2 className="skills__header">Skills</h2>
+      </div>
+    </div>
+  )
 }
