8de459f67cbd03dbd69317a832fceb2601e6a6eb
Tav Hafner <23143092+haftav@users.noreply.github.com>
2019-09-07 17:18:42 -0600

    a few edits and starting on contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import LinkedInIcon from './Contact/LinkedInIcon';
 
 export default () => {
     return (
         <div className='contact'>
             <h2 className="contact__header">Get In Touch.</h2>
             <div className="contact__iconsContainer">
-                <div className="contact__icon">icon1</div>
+                <div className="contact__icon">
+                    <LinkedInIcon />
+                </div>
                 <div className="contact__icon">icon2</div>
                 <div className="contact__icon">icon3</div>
             </div>
