34f2762760b9a6a37ca3f2405c55dbf36b88fe97
Tav Hafner <23143092+haftav@users.noreply.github.com>
2019-09-16 20:28:59 -0600

    skills divider looking good now

--- src/components/Skills/SkillsDivider.jsx
+++ src/components/Skills/SkillsDivider.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
+import React from 'react';
 
 const SkillsDivider = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 1919.207 213"
+    viewBox="0 0 1920 213.088"
     preserveAspectRatio="none"
     className="skills__divider"
   >
-    <g transform="translate(223 -3317)">
-      <rect
-        width="1919"
-        height="14"
+    <g id="Group_6" data-name="Group 6" transform="translate(223 -3317)">
+      <path
+        id="Path_39"
+        data-name="Path 39"
+        d="M0,0H1919.793V13l-33.867,5.458S722.4,38.918,243.48,38.745L0,13Z"
         transform="translate(-223 3317)"
         fill="#fff"
       />
       <path
-        d="M809.051,2602.014l753.028-122.905V2679Z"
-        transform="translate(134 851)"
+        id="Path_36"
+        data-name="Path 36"
+        d="M809.051,2602.068l753.028-122.959v199.979Z"
+        transform="translate(134.793 851)"
         fill="rgba(0,188,131,0.44)"
       />
       <path
-        d="M-356.784,2679l-.033-200,611.593,64.911Z"
+        id="Path_37"
+        data-name="Path 37"
+        d="M-356.784,2679.088-356.817,2479l6.518.692,605.076,64.248Z"
         transform="translate(134 851)"
         fill="rgba(37,90,74,0.45)"
       />
       <path
-        d="M-356.784,2479,809.524,2602.035l752.684-122.983Z"
+        id="Path_38"
+        data-name="Path 38"
+        d="M-356.784,2479,810.005,2602.035,1563,2479.052Z"
         transform="translate(134 851)"
         fill="#fff"
       />
     </g>
   </svg>
-)
+);
 
-export default SkillsDivider
+export default SkillsDivider;
