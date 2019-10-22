0d833e6e237bd2e3f13208b779f4af441b2b5164
Tav Hafner <23143092+haftav@users.noreply.github.com>
2019-10-21 20:56:23 -0600

    updating seo component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import Splash from '../components/Splash';
 import About from '../components/About';
@@ -8,12 +9,18 @@ import Contact from '../components/Contact';
 import '../assets/scss/main.scss';
 
 export default () => {
-    return (
-        <div className="portfolio" id="portfolio-wrapper">
-            <Splash />
-            <About />
-            <Skills />
-            <Contact />
-        </div>
-    )
-}
+  return (
+    <div className="portfolio" id="portfolio-wrapper">
+      <Helmet>
+      <html lang="en" />
+          <meta charSet="utf-8" />
+          <meta name="description" content="I'm a professional front end engineer with knowledge across the full stack of web development." />
+          <title>Tav Hafner Web Development</title>
+      </Helmet>
+      <Splash />
+      <About />
+      <Skills />
+      <Contact />
+    </div>
+  );
+};
